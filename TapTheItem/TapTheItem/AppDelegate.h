a4d1b851c1058d56140198378bb23bd0aa186dd7
John Russel Usi <j.usi@klab.com>
2014-10-22 16:24:52 +0800

    Started TapTheItem Project

---
+++ TapTheItem/TapTheItem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TapTheItem
+//
+//  Created by John Russel Usi on 10/22/14.
+//  Copyright (c) 2014 Klab Cyscorpions Training Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
